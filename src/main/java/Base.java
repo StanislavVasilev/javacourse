cac3c123640b4bbace7384e671d51447bc3f67a1
StanislavVasilev <svasilievalee@gmail.com>
2020-02-05 22:49:24 +0300

    added Base.class

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -1,5 +1,17 @@
+/**
+ * @author Stanislav
+ * @see Base
+ * @see #main(String[])
+ *
+ */
 public class Base {
+  /**
+   * @param args params in method
+   */
   public static void main(String[] args) {
+    for (String arg: args) {
+      System.out.println(arg);
+    }
 
   }
 }
