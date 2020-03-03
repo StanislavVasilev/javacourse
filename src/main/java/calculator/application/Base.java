351cfa4dd4358fbe5bd1157999bc4a8e82c3a1fe
StanislavVasilev <svasilievalee@gmail.com>
2020-03-03 20:12:19 +0300

    "Перемещены классы внутрь пакета application"

--- src/main/java/calculator/application/Base.java
+++ src/main/java/calculator/application/Base.java
@@ -1,3 +1,5 @@
+package calculator.application;
+
 /**
  * @author Stanislav
  * @see Base
--- src/main/java/calculator/application/MainClass.java
+++ src/main/java/calculator/application/MainClass.java
@@ -1,4 +1,4 @@
-package calculator;
+package calculator.application;
 
 import sun.usagetracker.UsageTrackerClient;
 
